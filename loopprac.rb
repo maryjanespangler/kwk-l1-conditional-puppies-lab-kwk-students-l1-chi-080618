d45176b738c2684cdf7a33e532730064e8202a58
maryjane spangler <maryjanespangler13@gmail.com>
2018-08-08 16:05:16 +0000

    automatically backed up by learn

--- loopprac.rb
+++ loopprac.rb
@@ -1,9 +1,16 @@
+# def friends
+#   num_friends = 0
+#   while <= 12
+#   puts "I have #{num_friends} frannnns."
+#   num_friends += 1
+# end
+# end
+
+# friends 
+
 def friends
   num_friends = 0
-  while <= 12
-  puts "I have #{num_friends} frannnns."
-  num_friends += 1
+until num_friends == 12  
+puts "I have #{num_friends} frannnnnns"
  end
 end
-
-friends 
